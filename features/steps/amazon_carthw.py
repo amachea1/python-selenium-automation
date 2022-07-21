5a142a8ee8b816e8773c93fc582db7503e2d1bf8
amachea1 <108307052+amachea1@users.noreply.github.com>
2022-07-20 19:01:12 -0700

    Updated

---
+++ features/steps/amazon_carthw.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+
+@given('Open Amazon website')
+def open_amazon(context):
+    context.driver.get('https://www.amazon.com/')
+
+@when('Clicking on Cart Icon')
+def click_cart(context):
+    context.driver.find_element(By.ID,'nav-cart-count').click()
+
+@then('Verify Your Amazon Cart Is Empty')
+def verify_amazon_cart(context):
+    context.driver.find_element(By.XPATH,"//div[@class='a-row sc-your-amazon-cart-is-empty']")
+    expected_result = '"Your Amazon Cart is empty"'
+    actual_result = context.driver.find_element(By.CSS_SELECTOR,'div.a-row.sc-your-amazon-cart-is-empty').text
+    assert expected_result == actual_result, f'Expected { expected_result} but got {actual_result}'
